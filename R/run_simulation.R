7c3ee6291b989a0867c42a89d1ede5a6f004ab2a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-02-01 16:24:09 +0100

    first working draft

--- R/run_simulation.R
+++ R/run_simulation.R
@@ -4,13 +4,62 @@
 #'   variant of \code{\link{expand.grid}} that allows running code at each
 #'   expansion
 #' @param params A named list of parameter vectors.
-#' @param experiment A function.
-#' @param preparer A named list of functions, defaults to \code{list()}.
+#' @param preparers A named list of functions, defaults to \code{list()}.
 #' @return An object of class \code{grex}.
 #' @examples
-#' run.simulation(list(a=1:3, b=2:5), function(x, y) list(z=x-y),
+#' run.simulation(list(a=1:2, b=2:4, experiment=TRUE),
 #'                list(a=function(a) list(x=a*a),
-#'                     b=function(a, b) if (b < 4) list(y=b-3) else list(y=a)))
+#'                     b=function(a, b) if (b < 4) list(y=b-3) else list(y=a),
+#'                     experiment=function(x, y) list(z=x-y)))
 #' @export
-run.simulation <- function(params, experiment, preparer=list()) {
+run.simulation <- function(params, preparers=list()) {
+  stopifnot(!is.null(names(params)))
+  stopifnot(names(params) == unique(names(params)))
+  stopifnot(length(preparers) == 0L || !is.null(names(preparers)))
+  stopifnot(length(setdiff(names(preparers), names(params))) == 0L)
+
+  params.len <- length(params)
+  param.frame <- data.frame(row.names=1)
+  envs <- list(list())
+
+  for (param.index in seq_along(params)) {
+    param <- params[[param.index]]
+    param.name <- names(params)[[param.index]]
+    param.len <- length(param)
+    stopifnot(param.len > 0L)
+
+    new.param.frame <- data.frame(param)
+    colnames(new.param.frame) <- param.name
+    new.param.frame <- merge(param.frame, new.param.frame, by=c())
+
+    new.envs <- vector(mode="list", length=nrow(new.param.frame))
+    for (row in seq_len(nrow(new.param.frame))) {
+      env.pos <- (row - 1L) %/% param.len + 1
+      env <- envs[[env.pos]]
+      if (!is.null(preparer <- preparers[[param.name]])) {
+        call.env <- c(env,
+                      as.list(new.param.frame[row, , drop=F]),
+                      params[seq(from=param.index + 1, length.out=params.len - param.index)])
+        preparer.args <- formals(preparer)
+        preparer.args.names <- names(preparer.args)
+        stopifnot(preparer.args.names %in% names(call.env))
+
+        return.env <- do.call(preparer, call.env[preparer.args.names])
+        stopifnot(!any(names(return.env) %in% colnames(new.param.frame)))
+        env[names(return.env)] <- return.env
+      }
+      new.envs[[row]] <- env
+    }
+
+    param.frame <- new.param.frame
+    envs <- new.envs
+  }
+
+  structure(
+    list(
+      grid=param.frame,
+      results=envs
+    ),
+    class="grex"
+  )
 }
